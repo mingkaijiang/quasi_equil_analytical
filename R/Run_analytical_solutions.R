c0dfe7032e102525c141d41ca7a79928485888e3
mingkaijiang <shanyujordan512@gmail.com>
2017-03-28 20:43:51 +1100

    added run analytical solution file

---
+++ R/Run_analytical_solutions.R
@@ -0,0 +1,11 @@
+
+#### Run analytical functions
+####
+#### 
+################################################################################
+
+#### Create a plots directory to store plots
+#if(!dir.exists("Plots"))dir.create("Plots")
+
+#### Source the prepare_R.R file to read in all the structures, libraries and functions
+source("R/prepare_R.R")
