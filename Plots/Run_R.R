59288e71bb027d642fa28646e638a401465cb64e
mingkaijiang <shanyujordan512@gmail.com>
2017-03-28 21:32:47 +1100

    added master script "Run_R.R"

---
+++ Plots/Run_R.R
@@ -0,0 +1,16 @@
+
+#### Main program
+####
+#### Level 1 code: to run the analytical solution code and the manuscript figure generating codes
+####
+#### Author: Mingkai Jiang (m.jiang@westernsydney.edu.au)
+#### 
+####
+################################################################################
+
+#### To run analytical solution codes
+source("R/Run_analytical_solutions.R")
+
+
+#### To generate manuscript figures
+source("Plots/Figure_generating.R")
