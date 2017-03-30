085aeb5c77db7b29f52e4f399868644088e45194
mingkaijiang <shanyujordan512@gmail.com>
2017-03-30 14:25:53 +1100

    cleaned some files

---
+++ GDAY/code/example/scripts/translate_GDAY_output_to_NCEAS_format.R
@@ -0,0 +1,7 @@
+#### Translate GDAY output file 
+#### Author: Mingkai Jiang
+#### Date Created: Mar-30-2017
+
+#### Match the NCEAS format and while we are at it carry out unit conversion so that 
+#### we matched required standard. Data should be comma-delimited
+
