ad2f5124d59a6737b5a1a59815a85cd39b549cd9
mingkaijiang <shanyujordan512@gmail.com>
2017-04-24 13:11:21 +1000

    made space for run 10

--- Functions/NConsLong.R
+++ Functions/NConsLong.R
@@ -214,6 +214,7 @@ NConsLong_variable_pass <- function(df, a, Nin=0.4, leachn=0.05, eqNPP,
     nleach <- leachn/(1-leachn) * (a$nfl*a$af + a$nr*(a$ar) + a$nw*a$aw)
     
     NPP_NC <- U0 * nuptakerate / (nwood + nburial + nleach)   # will be in g C m-2 yr-1
+    
     NPP_N <- NPP_NC*10^-3 # returned in kg C m-2 yr-1
     
     df <- data.frame(NPP_N, nwood,nburial,nleach,a$aw)
