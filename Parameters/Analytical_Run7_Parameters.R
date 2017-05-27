8be1190ab7969e79414a11d3c4bbb9d6a7767487
mingkaijiang <shanyujordan512@gmail.com>
2017-05-27 19:52:32 +1000

    created parameter directory for analytical solutions

---
+++ Parameters/Analytical_Run7_Parameters.R
@@ -0,0 +1,34 @@
+#### setting CO2 concentrations
+CO2_1 <- 350.0
+CO2_2 <- 700.0
+
+#### define parameters
+nwood = 0.005 
+pwood = 0.0003
+nrho = 0.7
+prho = 0.7
+nretrans = 0.5
+pretrans = 0.6
+nwvar = TRUE
+pwvar = TRUE
+LUE0=1.4
+I0=3
+Nref=0.04
+kext=0.5
+SLA=5
+sf=0.5
+cfrac = 0.45
+cue = 0.5
+leachn = 0.05
+leachp = 0.05
+Nin = 0.4
+Pin = 0.02
+k1=0.01
+k2=0.01
+k3=0.05
+Tsoil = 15
+Texture = 0.5
+ligfl = 0.2
+ligrl = 0.16
+pcp = 0.005
+ncp = 0.1
