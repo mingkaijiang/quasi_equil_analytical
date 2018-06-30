e3a63b2a0525d1144a819fb4977a9002a66b2365
mingkaijiang <shanyujordan512@gmail.com>
2018-06-30 18:57:33 +1000

    deleting codes

--- Functions/eqNC.R
+++ Functions/eqNC.R
@@ -8,18 +8,6 @@ eqNC <- function(nf, NPP, CO2, af) {
 }
 
 
-### NPP as function of nf and LAI (which is calculated from NPP),
-### Autotrophic respiration as a function of plant tissue N content
-eqNC_respiration <- function(df, NPP, co2, LUE0, Nref, I0, kext, SLA, ADF, sf, cfrac) {
-    
-    Ra <- Compute_Ra(a=ADF, NPP=NPP)
-    
-    ##Returns G: total C production (i.e. NPP)
-    return(LUE(df, co2, LUE0, Nref) * I0 * (1 - exp(-kext*SLA*ADF$af*NPP/sf/cfrac)) - Ra)
-    
-}
-
-
 ### NPP as function of nf and LAI (which is calculated from NPP)
 ### basic function: CUE dependent
 eqPC <- function(nf, pf, pfdf, NPP, CO2) {
@@ -58,29 +46,6 @@ eqPC_full_cn <- function(nf, nfdf, NPP, CO2) {
     
     ##Returns G: total C production (i.e. NPP)
     return( gpp * cue)
-    
-}
-
-### NPP as function of nf, pf and LAI (which is calculated from NPP),
-### Autotrophic respiration as a function of plant tissue N content
-eqPC_respiration <- function(nf, pf, nfdf, pfdf, NPP, CO2) {
-    
-    # LUE in umol C umol-1 PAR * umol PAR = umol C
-    lue_yr <- LUE_full_cnp_walker(nf, pfdf, pf, CO2, NPP*1000.0) * par
-    
-    # return ra as kg m-2 yr-1
-    Rd <- Compute_Rdark(nfdf, pfdf, NPP*1000.0)
-    
-    # assumes wood + root respiration = 0.5 leaf dark respiration
-    Ra <- Rd * 2.5
-    
-    # return gpp as kg m-2 yr-1
-    gpp <- lue_yr * (1 - exp(-kext*SLA*nfdf$af*NPP/sf/cfrac)) * conv * 365 / 1000.0
-
-    #browser()
-    
-    ##Returns G: total C production (i.e. NPP)
-    return(gpp - Ra)
     
 }
 
