f27060a1882fb0b07a7ed620035ab71686912828
mingkaijiang <shanyujordan512@gmail.com>
2018-06-30 18:49:02 +1000

    deleted un-needed codes for the manuscript.

--- Run_programs.R
+++ Run_programs.R
@@ -1,5 +1,7 @@
 
 #### Main program
+###  This is branch is for manuscript of CNP investigations only.
+### 
 ###
 ### Summary:
 ### 1. To prepare GDAY met forcing data and parameter files
@@ -28,13 +30,13 @@ source("R/prepare_R.R")
 #### ------------------------ Prepare GDAY stuffs -------------------------- #####
 ### Create met data for gday simulations
 #source("GDAY/pre_processing/create_monthly_met_for_GDAY.R")
-source("GDAY/pre_processing/create_daily_met_for_GDAY.R")
+#source("GDAY/pre_processing/create_daily_met_for_GDAY.R")
 
 ### compile gday program and send to simulation folders
-source("GDAY/pre_processing/Make_GDAY_and_Send_To_Folders.R")
+#source("GDAY/pre_processing/Make_GDAY_and_Send_To_Folders.R")
 
 ### Here need a script to modify the R scripts parameters for each simulations
-source("GDAY/pre_processing/Paste_R_script_to_folders.R")
+#source("GDAY/pre_processing/Paste_R_script_to_folders.R")
 
 
 #### ------------------------ Run GDAY simulations ------------------------- #####
@@ -43,37 +45,37 @@ source("GDAY/pre_processing/Paste_R_script_to_folders.R")
 ### Only needs GDAY simulation to verify the analytical solution
 ### Currently only implemented GDAY simulations for Run 1 - 3
 ### i.e. CNP with variable wood, CN with variable wood, CNP with fixed wood stoichiometry
-source("GDAY/pre_processing/Run_GDAY.R")
+#source("GDAY/pre_processing/Run_GDAY.R")
 
 
 #### ------------------ Post-processing GDAY simulations ------------------- #####
 ### Convert from monthly to annual data and save to analyses subfolders
 ### This step is the only "must-run" step for post-processing purpose
 #source("GDAY/post_processing/Convert_GDAY_monthly_to_annual.R")   
-source("GDAY/post_processing/Convert_GDAY_daily_to_annual.R")   
+#source("GDAY/post_processing/Convert_GDAY_daily_to_annual.R")   
 
 
 ### delete all raw GDAY output files as they are large
-source("GDAY/post_processing/delete_raw_gday_files.R")
+#source("GDAY/post_processing/delete_raw_gday_files.R")
 
 ### Mass balance checks, takes very long to run!
 # source("GDAY/post_processing/mass_balance.R")
 
 ### Plot time series spin up files for each simulations
-source("GDAY/post_processing/Transient_spin_up_plot.R")
+#source("GDAY/post_processing/Transient_spin_up_plot.R")
 
 ### Check spin-up and transient continuity
 ### co2_amb and co2_ele only pools, starting from last 10 years of equilibration
-source("GDAY/post_processing/Check_continuity.R")
+#source("GDAY/post_processing/Check_continuity.R")
 
 ### plot continuity starting from transient year 1 and specify endyear for pools and flxues
 ### Only for elevated CO2 runs
-source("GDAY/post_processing/Check_continuity_transient.R")
+#source("GDAY/post_processing/Check_continuity_transient.R")
 
 ### plot gday simulated quasi-equil points under aCO2 and eCO2
 ### Note: need to specify years when L and VL equilibrates
 ###       better to consider an automatic process to pick these years
-source("GDAY/post_processing/Plot_GDAY_quasi_equil_constraints.R")
+#source("GDAY/post_processing/Plot_GDAY_quasi_equil_constraints.R")
 
 
 #### ------------------------ Run analytical stuffs ------------------------ #####
@@ -85,7 +87,7 @@ source("R/Run_analytical_solutions.R")
 
 #### ------------- Checking GDAY matches with analytical results ----------- #####
 ### generate a table for comparison of the equilibrium points
-source("R/Check_analytical_gday_matches.R")
+#source("R/Check_analytical_gday_matches.R")
 
 # Note: VL does not 100% matched with GDAY simulated VL, possible reasons:
 # GDAY equilibrium definition: < 1e-2 difference might not mean equilibrium.
